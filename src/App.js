3d9bf5cabf67c4d278fc9001c964c51acc8ebd5e
UnixLoverSaurabh <saurabh4104@gmail.com>
2020-06-21 17:29:47 +0530

    Authentication structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './components/Layout/Layout'
 import Layout from './components/Layout/Layout';
+import Authentication from './containers/Authentication/Authentication';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Testing</p>
+          <Authentication />
         </Layout>
       </div>
     );
---
+++ src/containers/Authentication/Authentication.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Auxi'
+
+class Authentication extends Component {
+        render() {
+                return(
+                        <Aux>
+                                <div>SignUp</div>
+                                <div>SignIn</div>
+                        </Aux>
+                );
+        }
+}
+
+export default Authentication;
