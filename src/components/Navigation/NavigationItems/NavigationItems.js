fb7a83d9b27f2ed9faa5b7aaf6feca864d34119f
UnixLoverSaurabh <saurabh4104@gmail.com>
2020-06-22 20:28:03 +0530

    side drawer and mobile view

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,11 +2,11 @@ import React from 'react';
 import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
+const NavigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/" active>Sign In</NavigationItem>
         <NavigationItem link="/">Sign Out</NavigationItem>
     </ul>
 );
 
-export default navigationItems;
+export default NavigationItems;
