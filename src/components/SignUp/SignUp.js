d68b947f9e79e4391f94a5710e301defd36a88cf
UnixLoverSaurabh <saurabh4104@gmail.com>
2020-06-23 18:32:03 +0530

    Spinner while sending sign up information

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,30 +1,44 @@
 import React, { Component } from 'react';
 import classes from './SignUp.css';
 import axios from '../../axios-orders.js';
+import Spinner from '../UI/Spinner/Spinner';
 
 class SignUp extends Component {
 
         state = {
                 username: '',
-                password: ''
+                password: '',
+                loading: false
         }
 
         SubmitDataHandler = (props) => {
+                this.setState({
+                        loading: true
+                });
+                
                 const userInfo = {
                         username: this.state.username,
                         password: this.state.password
                 }
                 axios.post('/users.json', userInfo)
                         .then(response => {
-                                console.log(response);
+                                this.setState({
+                                        loading: false
+                                });
                         }).catch(error => {
-                                console.log(error);
+                                this.setState({
+                                        loading: false
+                                });
                         });
 
-                alert("Information submitted");
+                // alert("Information submitted");
         };
 
         render() {
+                let spin = null;
+                if(this.state.loading) {
+                        spin = <Spinner />
+                }
                 return (
                         <div className={classes.login}>
                                 <input type="text" placeholder="Username" value={this.state.username} id="username" onChange={(event) => this.setState({
@@ -33,10 +47,12 @@ class SignUp extends Component {
                                 <input type="password" placeholder="password" value={this.state.password} id="password" onChange={(event) => this.setState({
                                         password: event.target.value
                                 })} />
-                                <input onClick={this.SubmitDataHandler} type="submit" value="Sign Up"/>
+                                <input onClick={this.SubmitDataHandler} type="submit" value="Sign Up" />
+
+                                {spin}
                                 <a href="#" class="forgot">forgot password?</a>
                         </div>
-        
+
                 );
         }
 };
