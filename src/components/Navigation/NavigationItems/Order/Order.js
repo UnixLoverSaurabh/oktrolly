f3049fd4ad7a67da385d93b45643f82c9fba681b
UnixLoverSaurabh <saurabh4104@gmail.com>
2020-06-27 11:52:53 +0530

    All orders are fetched from firebase and rendered to screen

--- src/components/Navigation/NavigationItems/Order/Order.js
+++ src/components/Navigation/NavigationItems/Order/Order.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import classes from './Order.css';
 
-const Order = (props) => (
-        <div className={classes.Order}>
-                <p>Materials: Sand (1)</p>
-                <p>Price: <strong>100</strong></p>
-        </div>
-);
+const Order = (props) => {
+
+        const materials = [];
+        for (let materialName in props.materials) {
+                materials.push(
+                        {
+                                name: materialName,
+                                amount: props.materials[materialName]
+                        }
+                );
+        }
+
+        const materialOutput = materials.map(materialKey => {
+                return <span key={materialKey.name} style={{
+                        textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'
+        }} > {materialKey.name} ({materialKey.amount}) </span>;
+        });
+
+        return (
+                <div className={classes.Order}>
+                        <p>Materials: {materialOutput}</p>
+                        <p>Price: <strong>{Number.parseFloat(props.price).toFixed(2)}</strong></p>
+                </div>
+        )
+};
 
 export default Order;
--- src/containers/AllOrders/AllOrders.js
+++ src/containers/AllOrders/AllOrders.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 import Order from '../../components/Navigation/NavigationItems/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class AllOrders extends Component {
+        state = {
+                orders: [],
+                loading: true
+        }
+
+        componentDidMount() {
+                axios.get('/orders.json').then(response => {
+                        let fetchedOrders = [];
+                        for (let key in response.data) {
+                                fetchedOrders.push({
+                                        ...response.data[key],
+                                        id: key
+                                });
+                        }
+                        this.setState({
+                                orders: fetchedOrders,
+                                loading: false
+                        });
+                }).catch(error => {
+                        this.setState({
+                                loading: false
+                        });
+                });
+        };
+
         render() {
                 return (
                         <div>
-                                <Order />
-                                <Order />
+                             {this.state.orders.map(order => (
+                                     <Order key={order.id} materials={order.materials} price={order.price} />
+                             ))}   
                         </div>
                 );
         };
-;}
+        ;
+}
 
-export default AllOrders;
+export default withErrorHandler(AllOrders, axios);
